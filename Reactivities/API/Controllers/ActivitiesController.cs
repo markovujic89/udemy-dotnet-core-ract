1685d1cb4bab09cabd829144a8bde9a41a959ca5
Marko Vujic <markoczvvujic@hotmail.com>
2022-04-05 19:24:13 +0200

    add Seed and first [GET] controller actions

---
+++ Reactivities/API/Controllers/ActivitiesController.cs
@@ -0,0 +1,34 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    public class ActivitiesController: BaseApiController
+    {
+        private readonly DataContext _dataContext;
+
+        public ActivitiesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _dataContext.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivityById(Guid id)
+        {
+            return await _dataContext.Activities.FindAsync(id);
+        }
+    }
+}
