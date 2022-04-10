c3b54570dcccc17f5e8d41175d6e845bff956340
Marko Vujic <markoczvvujic@hotmail.com>
2022-04-10 19:34:14 +0200

    instal axios, first fatch API, add CROS policy

--- Reactivities/API/Startup.cs
+++ Reactivities/API/Startup.cs
@@ -33,6 +33,13 @@ namespace API
             {
                 options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                 {
+                     policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                 });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +55,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
