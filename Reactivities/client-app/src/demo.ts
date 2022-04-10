cb4d71d7c044c6bac2567a7d327a20c90166ba07
Marko Vujic <markoczvvujic@hotmail.com>
2022-04-10 13:44:16 +0200

    using typescript demo

---
+++ Reactivities/client-app/src/demo.ts
@@ -0,0 +1,26 @@
+// use typescript: enable variable to set more than one specific type
+let data:number|string = 42;
+
+data = 'tt';
+
+
+interface Duck{
+    name: string;
+    numberOfLegs: number;
+    makeSound?: (sound: any) => void;
+
+}
+const duck1:Duck = {
+    name: "duck1",
+    numberOfLegs: 2,
+    makeSound: (sound:string) => {console.log(sound)}
+}
+
+const duck2:Duck = {
+    name: "duck2",
+    numberOfLegs: 2,
+    makeSound: (sound:any) => {console.log(sound)}
+}
+
+// typescript: call object which is possible undefinde. Use ! to prevent error.
+duck1.makeSound!("sound");
